de5fac73d3d7783c72213a36d1d4c83c2bd524a0
Vincenzo Ferme <fermevincenzo@gmail.com>
2016-02-05 19:17:20 +0100

    Fixes a bug with the local container IP on Tutum

--- faban/agent/services/fix_localhost/fix_localhost.sh
+++ faban/agent/services/fix_localhost/fix_localhost.sh
@@ -18,5 +18,11 @@ fi
 #Update the localhost to the actual container IP on the network
 #We cannot direclty edit /ets/hosts with sed: https://github.com/smdahlen/vagrant-hostmanager/issues/136
 sed -r 's/127.0.0.1(([[:space:]]\+)|\t)localhost/'$ip' localhost/g' /etc/hosts > /etc/hosts.new
+#If on TUTUM, we also need to update the local IP of the container
+if [[ $TUTUM_CONTAINER_HOSTNAME ]]; then
+	CONTAINER_LOCAL_IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+    sed -ri 's/'$CONTAINER_LOCAL_IP'(([[:space:]]\+)|\t)'$TUTUM_CONTAINER_HOSTNAME'/'$ip' '$TUTUM_CONTAINER_HOSTNAME'/g' /etc/hosts.new
+fi
+
 cat /etc/hosts.new > /etc/hosts
 rm /etc/hosts.new
