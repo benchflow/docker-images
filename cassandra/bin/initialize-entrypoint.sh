63e253743bd3e8101117f86dfb1e260dad0eb018
Vincenzo Ferme <fermevincenzo@gmail.com>
2016-03-05 19:05:37 +0100

    Updates Cassandra to version 2.2.5

--- cassandra/bin/initialize-entrypoint.sh
+++ cassandra/bin/initialize-entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 echo "Starting Casandra to initialize it..."
 cassandra
 echo "Waiting for Cassandra to be ready..."
-sleep 10
+sleep 20
 echo "Initialising the database..."
 cqlsh -f /app/data/database.cql
 echo "Killing Cassandra..."
 pkill -f cassandra
-sleep 10
+sleep 20
 echo "Cassandra database initialised..."
 
 # execute the base image entrypoint in the context of the current shell
